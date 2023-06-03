0f41fbea96cc8c1635cb0d53d1eac58e35b2f40a
Web3Tester2023 <38856332+Web3Tester2023@users.noreply.github.com>
2023-06-03 18:44:05 +0800

    Update contractAddresses.ts

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,5 +1,5 @@
 export const nftDropContractAddress =
-  "0x";
+  "0x875B5D209D1F777f58aB6148aBcE01aD0712738C";
 export const tokenContractAddress =
   "0x5280F0CaCD724a3216eBfa762a7a7Ff925C2470D";
 export const stakingContractAddress =
